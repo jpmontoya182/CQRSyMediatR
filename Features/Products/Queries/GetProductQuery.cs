8cd7f3049ee1634f1d8ff7e6e21609e5fcd47a38
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2022-03-31 17:18:42 -0500

    Initial commit

---
+++ Features/Products/Queries/GetProductQuery.cs
@@ -0,0 +1,39 @@
+using MediatR;
+using CQRSyMediatR.Infrastruture.Persistence;
+
+namespace CQRSyMediatR.Features.Products.Queries;
+
+public class GetProductQuery : IRequest<GetProductQueryResponse>
+{
+    public int ProductId { get; set; }
+}
+
+// handler
+public class GetProductQueryHandler : IRequestHandler<GetProductQuery, GetProductQueryResponse>
+{
+    private readonly MyAppDbContext _context;
+
+    public GetProductQueryHandler(MyAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetProductQueryResponse> Handle(GetProductQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _context.Products.FindAsync(request.ProductId);
+
+        return new GetProductQueryResponse
+        {
+            Description = product.Description,
+            ProductId = product.ProductId,
+            Price = product.Price
+        };
+    }
+}
+
+public class GetProductQueryResponse
+{
+    public int ProductId { get; set; }
+    public string Description { get; set; } = default!;
+    public double Price { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Just for fun
+
+This project implements two patterns CQRS, and Mediator(with MediatR)
+
+*- On Info.txt is the source*
